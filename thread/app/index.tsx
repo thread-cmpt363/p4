162f5808f6536db22789b3171404a8be0f740323
Bianca Dimaano <bianca.fbcd@gmail.com>
2025-04-05 19:20:24 -0700

    Change calendar icon

--- thread/app/index.tsx
+++ thread/app/index.tsx
@@ -60,8 +60,21 @@ export default function Dashboard(){
           <View style={[styles.card, styles.dateCard, styles.leftCardMargin]}>
             <Text style={[styles.dayText, styles.poppinsSemibold]}>Monday</Text>
             <Text style={[styles.dateText, styles.poppinsSemibold]}>8</Text>
-            <Calendar size={24} strokeWidth={2.5} color="#1e1e1e" 
-            style={{position: "absolute", bottom:24, left:16}} />
+            {/* <Calendar size={24} strokeWidth={2.5} color="#1e1e1e" 
+            style={{position: "absolute", bottom:24, left:16}} /> */}
+            <View style={{position: "absolute", bottom:24, left:16}}>
+              <Svg
+                width={23}
+                height={25}
+                viewBox="0 0 23 25"
+                fill="none"
+              >
+                <Path
+                  d="M19.6221 2.86777H18.4221V0.467773H16.0221V2.86777H6.42212V0.467773H4.02212V2.86777H2.82212C1.49012 2.86777 0.434119 3.94777 0.434119 5.26777L0.422119 22.0678C0.422119 23.3878 1.49012 24.4678 2.82212 24.4678H19.6221C20.9421 24.4678 22.0221 23.3878 22.0221 22.0678V5.26777C22.0221 3.94777 20.9421 2.86777 19.6221 2.86777ZM19.6221 22.0678H2.82212V10.0678H19.6221V22.0678ZM7.62212 14.8678H5.22212V12.4678H7.62212V14.8678ZM12.4221 14.8678H10.0221V12.4678H12.4221V14.8678ZM17.2221 14.8678H14.8221V12.4678H17.2221V14.8678ZM7.62212 19.6678H5.22212V17.2678H7.62212V19.6678ZM12.4221 19.6678H10.0221V17.2678H12.4221V19.6678ZM17.2221 19.6678H14.8221V17.2678H17.2221V19.6678Z"
+                  fill="#1E1E1E"
+                />
+              </Svg>
+            </View>
           </View>
 
           {/* Weather Card */}
